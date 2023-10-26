93f6fdb25158096cbea31d7ed955baecbf3f168c
gersonhiggins <gahg.10gerson@gmail.com>
2023-10-26 10:54:07 -0400

    update nav-bar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <nav>
-      <ul>
+    <nav className='dropdown'>
+      <button className={`dropbtn ${isOpen ? 'active' : ''}`} onClick={toggleDropdown}>
+        <span className={`dropbtn__icon1 ${isOpen ? 'active' : ''}`} onClick={toggleDropdown}></span>
+        <span className={`dropbtn__icon2 ${isOpen ? 'active' : ''}`} onClick={toggleDropdown}></span>
+      </button>
+      <ul className={`list ${isOpen ? 'active' : ''}`} onClick={toggleDropdown}>
+        <li>
+          <img className="logo" src="https://www.freepnglogos.com/uploads/logo-home-png/vector-brush-home-logo-download-vector-logos-6.png" alt="logo" />
+        </li>
         <li>
           <Link to="/">Home</Link>
         </li>
