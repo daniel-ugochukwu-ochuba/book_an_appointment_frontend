7dc0fe44d3c1111afdcecf2f441b05eacacf172c
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-29 22:20:06 +0200

    Implement the login component

---
+++ src/components/auth/login.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from '../../redux/actions';
+
+const Login = () => {
+  const dispatch = useDispatch();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    dispatch(login(username, password));
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button onClick={handleLogin} type="button">Login</button>
+    </div>
+  );
+};
+
+export default Login;
