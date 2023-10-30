90ac09726ba9103d189104406cbcef8de1accff3
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-30 08:11:57 +0200

    handle error in the login and navigate to houses in sucess case

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { login } from '../../redux/actions';
 
 const Login = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-  const handleLogin = () => {
-    dispatch(login(username, password));
+  const handleLogin = async () => {
+    try {
+      await dispatch(login(username, password));
+      navigate('/houses'); // Redirect to /houses upon successful login
+    } catch (error) {
+      setError(error.message); // Set the error message received from the action
+    }
   };
 
   return (
@@ -26,6 +34,7 @@ const Login = () => {
         placeholder="Password"
       />
       <button onClick={handleLogin} type="button">Login</button>
+      {error && <p>{error}</p>}
     </div>
   );
 };
