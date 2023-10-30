a765ba12c8ea7d8a3d8c4b4854891b5eceaac078
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-30 08:17:44 +0200

    Fixed the cookie in case it's undefined

--- src/components/auth/private-route.js
+++ src/components/auth/private-route.js
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
 const PrivateRoute = () => {
-  const isAuthenticated = Cookies.get('token') !== null;
+  const isAuthenticated = Cookies.get('token');
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
