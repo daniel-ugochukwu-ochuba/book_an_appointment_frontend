b7a36d614966ec4bf452a5455061a459f9037474
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-30 08:12:34 +0200

    set the private route based on cookies

--- src/components/auth/private-route.js
+++ src/components/auth/private-route.js
@@ -1,45 +1,10 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import Cookies from 'js-cookie';
 
 const PrivateRoute = () => {
-  const isAuthenticated = useSelector((state) => state.authentication === null);
+  const isAuthenticated = Cookies.get('token') !== null;
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
-
-// import React from 'react';
-// import { Route } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
-// import PropTypes from 'prop-types';
-
-// const PrivateRoute = ({ component: Component, ...rest }) => {
-//   const isAuthenticated = useSelector((state) => state.authentication !== null);
-
-//   return (
-//     <Route
-//       {...rest}
-//       component={(props) => (
-//         isAuthenticated ? (
-//           <div>
-//             <Component {...props} />
-//           </div>
-//         ) : (
-//           <h1>Not logged in</h1>
-//         )
-//       )}
-//     />
-
-//   // <Route
-//   //   {...rest}
-//   //   render={(props) => (
-//   //     isAuthenticated ? (
-//   //       <Component {...props} />
-//   //     ) : (
-//   //       <h1>Not logged in</h1>
-//   //     )
-//   //   )}
-//   // />
-//   );
-// };
 
 export default PrivateRoute;
