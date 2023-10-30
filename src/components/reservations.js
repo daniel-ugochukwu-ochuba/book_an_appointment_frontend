3ae25086271861d6aa38abf425625f8107f0decd
Daniel Ochuba <danielochuba78@gmail.com>
2023-10-30 18:29:42 +0100

    add dummy house data

---
+++ src/components/reservations.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const array = [
+  {
+    houseId: '101',
+    houseName: 'Cozy Cabin Retreat',
+    location: 'Mountain View, CA',
+    numOfBedrooms: 2,
+    numOfBathrooms: 1.5,
+    maxOccupancy: 4,
+    amenities: ['Wi-Fi', 'Fireplace', 'Hot Tub', 'Scenic View'],
+    description: 'Escape to this charming cabin nestled in the heart of the mountains. Enjoy breathtaking views, a cozy fireplace, and a private hot tub.',
+  },
+  {
+    houseId: '102',
+    houseName: 'Seaside Cottage',
+    location: 'Beachfront, Malibu, CA',
+    numOfBedrooms: 3,
+    numOfBathrooms: 2,
+    maxOccupancy: 6,
+    amenities: ['Beach Access', 'Ocean View', 'Outdoor Grill'],
+    description: 'Experience coastal living in this beautiful seaside cottage. Walk to the beach, enjoy ocean views, and BBQ on the spacious deck.',
+  },
+  {
+    houseId: '103',
+    houseName: 'Urban Loft',
+    location: 'Downtown, New York City, NY',
+    numOfBedrooms: 1,
+    numOfBathrooms: 1,
+    maxOccupancy: 2,
+    amenities: ['City View', 'Walking Distance to Attractions'],
+    description: "Live in style in this modern urban loft in the heart of NYC. Enjoy stunning city views and easy access to the city's attractions.",
+  },
+];
+
+
