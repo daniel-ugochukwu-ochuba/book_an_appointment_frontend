63306b5ae5d8987b1a333549e34734b0de1a8886
Daniel Ochuba <danielochuba78@gmail.com>
2023-10-31 17:29:50 +0100

    modify reservations to use swiper

--- src/components/reservation.js
+++ src/components/reservation.js
@@ -2,9 +2,14 @@
 
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {
+  Navigation, Pagination, Scrollbar, A11y,
+} from 'swiper/modules';
 import { fetchReservation } from '../redux/reservation/reservationSlice';
 
 function Reservation() {
+  const slides = Array.from({ length: 10 }, (_, index) => index + 1);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -15,32 +20,58 @@ function Reservation() {
   console.log(reservations);
   return (
     <div className="slideTemplate">
-      <div className="reserved-item">
-        <img src="https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_1280.jpg" className="reserved-image" alt="house" />
-        <div className="reserved-info">
-          <h2 className="reserved-name">Urban Loft</h2>
-          <p className="reserved-description">Experience coastal living in this beautiful seaside cottage. Walk to the beach, enjoy ocean views, and BBQ on the spacious deck.</p>
-          <p className="reserved-location">
-            {' '}
-            <svg xmlns="http://www.w3.org/2000/svg" className="location" height="1em" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" /></svg>
-            Downtown, New York City, NY
-          </p>
-          <p className="reserved-price">$250/night</p>
-          <div className="days-info">
-            <p className="reserved-date">
-              Start:
-              {' '}
-              <em>20 oct 2023</em>
-              {' '}
-            </p>
-            <p className="days">
-              Days:
-              {' '}
-              <strong>23</strong>
-            </p>
-          </div>
-        </div>
-      </div>
+      <Swiper
+        className="swiper"
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={30}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        navigation={{
+          prevEl: '.prev',
+          nextEl: '.next',
+        }}
+      >
+        {slides.map((slide) => (
+          <SwiperSlide key={slide} style={{ display: 'flex' }} className="house">
+            <div className="circle">
+              <div className="house-img" />
+            </div>
+            <div className="details">
+              <h3>NAME</h3>
+              <p>address: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p>description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p>Price Per Day: 100$</p>
+              <div className="days-info">
+                <p className="reserved-date">
+                  Start:
+                  {' '}
+                  <em>20 oct 2023</em>
+                  {' '}
+                </p>
+                <p className="days">
+                  Days:
+                  {' '}
+                  <strong>23</strong>
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+        <div className="prev">«</div>
+        <div className="next">»</div>
+      </Swiper>
     </div>
   );
 }
