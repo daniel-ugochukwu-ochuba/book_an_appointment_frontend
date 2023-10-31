adbabd0b21ae9aeb281287426f9ca36eff8e0e97
Daniel Ochuba <danielochuba78@gmail.com>
2023-10-31 17:30:42 +0100

    render modified reservation file

--- src/components/reservations.js
+++ src/components/reservations.js
@@ -1,81 +1,20 @@
-/* eslint-disable radix */
-/* eslint-disable no-use-before-define */
-import React, { useEffect } from 'react';
+import '../assests/stylesheets/houses.css';
+import React from 'react';
 import Reservation from './reservation';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css';
+import '../assests/stylesheets/reserve.css';
 
 function Reservations() {
-  useEffect(() => {
-    const nextBtn = document.querySelector('.right-nav');
-    const prevBtn = document.querySelector('.left-nav');
-    const container = document.querySelector('.mainContainerCarouselFinal');
-    const slide = document.querySelector('.slideTemplate');
-
-    let nbclick = 0;
-    const nbSlide = 10;
-
-    if (!nextBtn || !prevBtn || !container || !slide) {
-      return;
-    }
-
-    function updateScroll() {
-      const screenWidth = parseInt(
-        getCS(container).getPropertyValue('--screenWidth').replace('px', ''),
-      );
-      const slideWidth = parseInt(
-        getCS(slide).getPropertyValue('--slideWidth').replace('px', ''),
-      );
-      const scrollAmount = ((screenWidth - slideWidth) / 1) * nbclick;
-      container.scroll({ left: scrollAmount, behavior: 'smooth' });
-    }
-
-    prevBtn.addEventListener('click', () => {
-      if (nbclick > 0) {
-        nbclick -= 1;
-        updateScroll();
-      }
-    });
-
-    // eslint-disable-next-line no-unused-vars
-    const { style: containerStyle, getComputedStyle: getCS } = window;
-    nextBtn.addEventListener('click', () => {
-      if (nbclick < nbSlide - 1) {
-        nbclick += 1;
-        updateScroll();
-      }
-    });
-  }, []);
   return (
     <>
-      <h1 className="reservation-title"> Reservations</h1>
-      <div className="containerVertical">
-        <div className="slideContainer" data-interval="false">
-          <div className="mainContainerCarouselFinal">
-            <Reservation />
-            <Reservation />
-            <Reservation />
-            <Reservation />
-            <Reservation />
-          </div>
-        </div>
-        <div className="controlerFinalCarousel">
-          <div className="left-nav">
-            <img
-              src="https://www.htmlcssbuttongenerator.com/image/arrowLeft.png"
-              height="12px"
-              width="12px"
-              alt="controler to the right"
-            />
-          </div>
-          <div className="right-nav">
-            <img
-              src="https://www.htmlcssbuttongenerator.com/image/arrowLeft.png"
-              height="12px"
-              width="12px"
-              alt="controler to the right"
-            />
-          </div>
-        </div>
-      </div>
+      <section className="houses-section">
+        <h1 className="reservation-title" style={{ textAlign: 'center' }}> Reservations</h1>
+
+        <Reservation />
+      </section>
     </>
   );
 }
