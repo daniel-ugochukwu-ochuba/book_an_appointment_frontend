7d37f80ec134f9ee8d33d7eedfa6c21d0d102081
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-31 19:19:17 +0200

    added redux actions for add house

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -4,6 +4,7 @@ import Cookies from 'js-cookie';
 const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 const REGISTRATION_SUCCESS = 'REGISTRATION_SUCCESS';
 const STORE_USER_DATA = 'STORE_USER_DATA';
+const ADD_HOUSE_SUCCESS = 'ADD_HOUSE_SUCCESS';
 
 export const loginSuccess = (token) => ({
   type: LOGIN_SUCCESS,
@@ -18,6 +19,11 @@ export const registrationSuccess = (token) => ({
 export const storeUserData = (userData) => ({
   type: STORE_USER_DATA,
   payload: userData,
+});
+
+export const addHouseSuccess = (houseData) => ({
+  type: ADD_HOUSE_SUCCESS,
+  payload: houseData,
 });
 
 export const login = (username, password) => async (dispatch) => {
@@ -43,6 +49,25 @@ export const register = (username, password) => async (dispatch) => {
     Cookies.set('token', token, { expires: 7 });
 
     dispatch(registrationSuccess(token));
+  } catch (error) {
+    if (error.response && error.response.data && error.response.data.error) {
+      throw new Error(error.response.data.error);
+    } else {
+      throw new Error('Registration failed');
+    }
+  }
+};
+
+export const addHouse = (houseData) => async (dispatch) => {
+  try {
+    const token = Cookies.get('token');
+
+    const response = await axios.post('http://127.0.0.1:3000/api/v1/houses', houseData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    dispatch(addHouseSuccess(response.data));
   } catch (error) {
     if (error.response && error.response.data && error.response.data.error) {
       throw new Error(error.response.data.error);
