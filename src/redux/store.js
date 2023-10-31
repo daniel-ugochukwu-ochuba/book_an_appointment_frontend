a8432c84c70f9885a1cd5edbf72c7d7e0d6e3267
Daniel Ochuba <danielochuba78@gmail.com>
2023-10-31 15:13:38 +0100

    add reservations to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducers from './reducers';
+import reservationReducer from './reservation/reservationSlice';
 
 export default configureStore({
   reducer: {
     reducer: reducers,
+    reservations: reservationReducer,
   },
 });
