29c9957cb6d66033904387d4798095dc7521fcfd
gersonhiggins <gahg.10gerson@gmail.com>
2023-10-31 10:50:38 -0400

    Fixes on Heouses and NavBar

--- src/components/houses.js
+++ src/components/houses.js
@@ -10,118 +10,47 @@ import 'swiper/css/scrollbar';
 import 'swiper/css';
 
 function Houses() {
+  const slides = Array.from({ length: 10 }, (_, index) => index + 1);
+
   return (
-    <section>
+    <section className="houses-section">
       <h2>Houses</h2>
       <Swiper
         className="swiper"
         modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={50}
-        slidesPerView={3}
+        spaceBetween={30}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
         navigation={{
           prevEl: '.prev',
           nextEl: '.next',
         }}
       >
-        <SwiperSlide className="house">
-          <div className="circle">
-            <div className="house-img" />
-          </div>
-          <div className="details">
-            <h3>NAME</h3>
-            <p>address: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>Price Per Day: 100$</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="house">
-          <div className="circle">
-            <div className="house-img" />
-          </div>
-          <div className="details">
-            <h3>NAME</h3>
-            <p>address: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>Price Per Day: 100$</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="house">
-          <div className="circle">
-            <div className="house-img" />
-          </div>
-          <div className="details">
-            <h3>NAME</h3>
-            <p>address: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>Price Per Day: 100$</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="house">
-          <div className="circle">
-            <div className="house-img" />
-          </div>
-          <div className="details">
-            <h3>NAME</h3>
-            <p>address: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>Price Per Day: 100$</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="house">
-          <div className="circle">
-            <div className="house-img" />
-          </div>
-          <div className="details">
-            <h3>NAME</h3>
-            <p>address: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>Price Per Day: 100$</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="house">
-          <div className="circle">
-            <div className="house-img" />
-          </div>
-          <div className="details">
-            <h3>NAME</h3>
-            <p>address: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>Price Per Day: 100$</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="house">
-          <div className="circle">
-            <div className="house-img" />
-          </div>
-          <div className="details">
-            <h3>NAME</h3>
-            <p>address: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>Price Per Day: 100$</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="house">
-          <div className="circle">
-            <div className="house-img" />
-          </div>
-          <div className="details">
-            <h3>NAME</h3>
-            <p>address: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>Price Per Day: 100$</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="house">
-          <div className="circle">
-            <div className="house-img" />
-          </div>
-          <div className="details">
-            <h3>NAME</h3>
-            <p>address: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>Price Per Day: 100$</p>
-          </div>
-        </SwiperSlide>
+        {slides.map((slide) => (
+          <SwiperSlide key={slide} style={{ display: 'flex' }} className="house">
+            <div className="circle">
+              <div className="house-img" />
+            </div>
+            <div className="details">
+              <h3>NAME</h3>
+              <p>address: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p>description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p>Price Per Day: 100$</p>
+            </div>
+          </SwiperSlide>
+        ))}
         <div className="prev">«</div>
         <div className="next">»</div>
       </Swiper>
