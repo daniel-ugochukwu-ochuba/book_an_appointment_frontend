af3c2582d31a5ac3dccab5594b2bf0630a7c6f81
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-11-01 13:40:59 +0200

    Setup react-toastify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Navbar from './components/navbar';
 import Home from './components/home';
 import Houses from './components/houses';
@@ -7,6 +8,7 @@ import Login from './components/auth/login';
 import Register from './components/auth/register';
 import PrivateRoute from './components/auth/private-route';
 import AddHouse from './components/houses/add-house';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
 function App() {
@@ -26,6 +28,7 @@ function App() {
               <Route path="/add-house" element={<AddHouse />} />
             </Route>
           </Routes>
+          <ToastContainer />
         </main>
       </>
     </>
