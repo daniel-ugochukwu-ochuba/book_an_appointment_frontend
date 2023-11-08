5f75e28651d8f67d1dbdcc01a6b3de70f244f237
Daniel Ochuba <danielochuba78@gmail.com>
2023-11-08 01:34:53 +0100

    refactor home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import '../assests/stylesheets/home.css';
 
-function Home() {
-  return (
-    <section className="home">
-      <div className="container">
-        <h1>House Rental</h1>
-      </div>
-    </section>
-  );
-}
+const Home = () => (
+  <section className="home">
+    <div className="container">
+      <h1>House Rental</h1>
+    </div>
+  </section>
+);
 
 export default Home;
